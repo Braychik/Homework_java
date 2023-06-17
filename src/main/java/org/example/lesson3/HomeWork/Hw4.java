5006c57d5fc3f336e4b008b1a129898ee1c017f8
Николай <ibraevnikolay@mail.ru>
2023-06-17 20:34:31 +0500

    Задичи по 3 семинару и ДЗ(переименовал 4 задание)

--- src/main/java/org/example/lesson3/HomeWork/Hw4.java
+++ src/main/java/org/example/lesson3/HomeWork/Hw4.java
@@ -2,10 +2,9 @@ package org.example.lesson3.HomeWork;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
-public class ex4 {
+public class Hw4 {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
         double avg = getAverage(numbers);
